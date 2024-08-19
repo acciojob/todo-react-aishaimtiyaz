fb9ae6d07f0616fd0941724e192f9de9b42643cb
Aisha Imtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2024-08-19 18:30:11 +0000

    changed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,40 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [todo,setTodo] = useState("");
+  const [todoList,setTodoList] = useState([]);
+  const [id,setId] = useState(1);
+
+  function addTodo(e)
+  {
+    e.preventDefault();
+    setTodoList([...todoList,{id:id,title:todo}]);
+    setTodo("");
+    setId(id+1);
+  }
+
+  function deleteTodo(delete_id)
+  {
+    setTodoList(todoList.filter((item)=> item.id != delete_id))
+  }
   return (
+  
     <div>
-        {/* Do not remove the main div */}
+      <h1>To-Do List</h1>
+        <form onSubmit={addTodo}>
+          <input type="text" placeholder="Enter to do" onChange={(e)=>setTodo(e.target.value)} value={todo} />
+          <button type="submit">Add to do</button>
+        </form>
+        <ul>
+          {todoList.map((item)=>{
+            return(<li key={item.id}>
+               {item.title}
+               <button onClick={()=> deleteTodo(item.id)}>Delete</button>
+            </li>);
+          })}
+        </ul>
     </div>
   )
 }
